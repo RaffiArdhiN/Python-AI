6e2a9f97e7a311fd11207dbdd8076e5188e4f0ad
Raffi Ardhi Naufal <ardhiraffi0@gmail.com>
2024-12-08 00:02:53 +0700

    First Commit

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
+from groq import Groq
 from flask import Flask, render_template, request
 from datetime import datetime
 
 app = Flask(__name__)
+
+AI_KEY = "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"
+
+client = Groq(api_key=AI_KEY)
+
+def ai_call(year):
+    try:
+        chat_completion = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": f"berikan saya 1 fakta menarik seputar teknologi di tahun {year}",
+                }
+            ],
+            model = "llama-3.2-1b-preview",
+            stream = False
+        )
+        ai_output = chat_completion.choices[0].message.content
+        return ai_output
+    
+    except Exception:
+        return f"Maaf Lag"
 
 @app.route('/')
 def main():
@@ -14,7 +37,9 @@ def cek_usia():
         tahun_lahir = int(request.form['tahun_lahir'])
         tahun_sekarang = datetime.now().year
         usia = tahun_sekarang - tahun_lahir
-        return render_template('cek_usia.html', usia=usia, tahun_lahir=tahun_lahir)
+        ai_output = ai_call(tahun_lahir)
+        # print(ai_output)
+        return render_template('cek_usia.html', usia=usia, tahun_lahir=tahun_lahir, ai_output=ai_output)
     return render_template('cek_usia.html', usia= None)
     
 if __name__ == "__main__":
